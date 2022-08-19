62b6714e731ca59b05779ee6f06569f755c164b7
Fedos16 <41968433+Fedos16@users.noreply.github.com>
2022-08-19 15:44:46 +0300

    test

---
+++ static/precache-manifest.77ae7634d2ce71b2a842d50802e719f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c2d17f5552c484b3060c02f166bdb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8a184f4632f5d82bd8",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7d41c44e644729815b63",
+    "url": "/static/js/2.da410c67.chunk.js"
+  },
+  {
+    "revision": "f24b473656bdcdf7a40306c1c14414ef",
+    "url": "/static/js/2.da410c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8a184f4632f5d82bd8",
+    "url": "/static/js/main.5928d66f.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
